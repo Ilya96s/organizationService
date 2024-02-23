8c032365a9ed4733e6849b73e8180dad53923cc6
Ilya96s <i.kaltygin@icloud.com>
2024-02-23 17:22:05 +0700

    enable eureka client

--- src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
+++ src/main/java/com/optimagrowth/organization/OrganizationServiceApplication.java
@@ -2,8 +2,12 @@ package com.optimagrowth.organization;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
+@RefreshScope
+@EnableDiscoveryClient
 public class OrganizationServiceApplication {
 
 	public static void main(String[] args) {
