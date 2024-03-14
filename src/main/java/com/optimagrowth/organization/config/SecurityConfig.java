07b761495922717ee2f74bd6caf2e1ef1b919277
iskaltygin <iskaltygin@fintechiq.ru>
2024-03-14 20:28:22 +0700

    add spring security configuration

---
+++ src/main/java/com/optimagrowth/organization/config/SecurityConfig.java
@@ -0,0 +1,75 @@
+package com.optimagrowth.organization.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
+
+        return http
+                .authorizeHttpRequests(authorizeRequests ->
+                        authorizeRequests
+                                .requestMatchers(HttpMethod.GET, "/v1/organization/**").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.PUT, "/v1/organization/**").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.POST, "/v1/organization/**").hasAnyRole("ADMIN", "USER")
+                                .requestMatchers(HttpMethod.DELETE, "/v1/organization/**").hasAnyRole("ADMIN")
+                                .anyRequest().authenticated()
+                )
+                .build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var converter = new JwtAuthenticationConverter();
+        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        converter.setPrincipalClaimName("preferred_username");
+        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+            var roles = (List<String>) jwt.getClaimAsMap("realm_access").get("roles");
+
+            return Stream.concat(authorities.stream(),
+                            roles.stream()
+                                    .filter(role -> role.startsWith("ROLE_"))
+                                    .map(SimpleGrantedAuthority::new)
+                                    .map(GrantedAuthority.class::cast))
+                    .toList();
+        });
+
+        return converter;
+    }
+
+//    @Bean
+//    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        var converter = new JwtAuthenticationConverter();
+//        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//        converter.setPrincipalClaimName("preferred_username");
+//        converter.setJwtGrantedAuthoritiesConverter(jwt -> {
+//            var authorities = jwtGrantedAuthoritiesConverter.convert(jwt);
+//            var roles = jwt.getClaimAsStringList("ostock_roles");
+//
+//            return Stream.concat(authorities.stream(),
+//                            roles.stream()
+//                                    .filter(role -> role.startsWith("ROLE_"))
+//                                    .map(SimpleGrantedAuthority::new)
+//                                    .map(GrantedAuthority.class::cast))
+//                    .toList();
+//        });
+//
+//        return converter;
+//    }
+}
